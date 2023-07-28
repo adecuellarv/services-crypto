ac5fa0954b2b9503a630991c52f0e3e65f179b0c
adecuellarv <76180286+adecuellarv@users.noreply.github.com>
2023-07-28 04:44:52 -0600

    try catch

--- src/services/index.js
+++ src/services/index.js
@@ -3,19 +3,23 @@ const axios = require('axios');
 async function getAssets() {
     const coins = ['bitcoin', 'ethereum', 'cardano'];
     const arrayAssets = [];
-    for (const coin of coins) {
-        const infoAsset = await axios.get(`https://data.messari.io/api/v1/assets/${coin}/metrics`);
-        if (infoAsset?.data?.data) {
-            const obj = infoAsset?.data?.data;
+    try {
+        for (const coin of coins) {
+            const infoAsset = await axios.get(`https://data.messari.io/api/v1/assets/${coin}/metrics`);
+            if (infoAsset?.data?.data) {
+                const obj = infoAsset?.data?.data;
 
-            //Mock of price, cuze the api don't have the last information
-            const price_usd = parseFloat(obj?.market_data?.price_usd);
-            const mockNumber = Math.round(Math.random() * 10);
+                //Mock of price, cuze the api don't have the last information
+                const price_usd = parseFloat(obj?.market_data?.price_usd);
+                const mockNumber = Math.round(Math.random() * 10);
 
-            obj.market_data.price_usd = price_usd + mockNumber;
-            arrayAssets.push(obj);
-        }
-    };
+                obj.market_data.price_usd = price_usd + mockNumber;
+                arrayAssets.push(obj);
+            }
+        };
+    } catch (error) {
+        console.error(error);
+    }
 
     return arrayAssets;
 }
